b120953cc93a3ff37e454f39aeda391611f0d232
thewilkybarkid <chriswilkinson84@gmail.com>
2012-11-12 11:48:40 +0000

    Add license

--- MisdGuzzleBundle.php
+++ MisdGuzzleBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony2 GuzzleBundle.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Misd\GuzzleBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
